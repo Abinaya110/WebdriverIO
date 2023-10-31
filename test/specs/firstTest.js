921df1c61019533fddb12b69ed93bbb2519a5ce0
abinaya110 <abi110guna@gmail.com>
2023-10-31 14:42:11 +0530

    test

--- test/specs/firstTest.js
+++ test/specs/firstTest.js
@@ -3,7 +3,10 @@ describe('Web application', async () => {
         const domain = "twinmo.ai";
 
 
-        await browser.url("https://twinmo.ai/");
+        await browser.url("https://twinmo.ai/services");
         console.log(await browser.getTitle());
+
+        await expect(browser).toHaveTitleContaining("Tailored Support for Product Development | Twinmo Services")
     });
 });
+  
